ed541c9737ae3a774fb70099e3d41d2b97aa70d3
MORAIS157 <djonatan.morais@escola.pr.gov.br>
2025-10-28 10:45:03 -0300

    28/10

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ campoResultado.textContent = palavrasChave.join(", ")
 function processaTexto(texto){
 let palavras = texto.split(/\P{L}+/u);
 
+const frequencias = contaFrequencias(palavras);
+let ordenadas = Object.keys(frequencias).sorte(ordenaPalavra;)
+
+function ordenaPalavras(p1, p2){
+    return frequencias [p2]-frequencias[p1];
+}
+  return ordenadas.slice(0, 10);
+
+
+}
+
+function contaFrequencias(palavras){
+
 let frequencias = {};
 
 for (let i of palavras){
@@ -27,5 +40,8 @@ for (let i of palavras){
 }
 
 return palavras;
+
+
+
 
 }
