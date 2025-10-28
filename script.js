0f9ca39930d2fdcd61af80057ce5b7afa312cd90
MORAIS157 <djonatan.morais@escola.pr.gov.br>
2025-10-28 10:47:03 -0300

    28/10

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function processaTexto(texto){
 let palavras = texto.split(/\P{L}+/u);
 
 const frequencias = contaFrequencias(palavras);
-let ordenadas = Object.keys(frequencias).sorte(ordenaPalavra;)
+let ordenadas = Object.keys(frequencias).sorte(ordenaPalavra);
 
 function ordenaPalavras(p1, p2){
     return frequencias [p2]-frequencias[p1];
