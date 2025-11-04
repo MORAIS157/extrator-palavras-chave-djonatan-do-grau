4efd8dd3917714f82339de7ce8d1691b9a8d29c5
MORAIS157 <djonatan.morais@escola.pr.gov.br>
2025-11-04 10:21:20 -0300

    04/11

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ function mostraPalavrasChave() {
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
 
-    for (let i in palavras){
+    for (let i in palavras) {
 
         palavras[i] = palavras[i].toLowerCase();
     }
     palavras = tiraPalavrasRuins(palavras);
 
-const frequencias = contaFrequencias(palavras);
-let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
-function ordenaPalavra(p1, p2) {
-    return frequencias[p2] - frequencias[p1];
-}
-return ordenadas.slice(0, 10);
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+    return ordenadas.slice(0, 10);
 }
 
 
@@ -47,7 +47,14 @@ function contaFrequencias(palavras) {
 
 }
 
-function tiraPalavrasRuins(palavras){
-const PALAVRAS_RUINS = new set(["para", "nós", "das", "como", "que" ];)
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new set(["para", "nós", "das", "como", "que"];)
+    const palavrasBoas = [];
 
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
